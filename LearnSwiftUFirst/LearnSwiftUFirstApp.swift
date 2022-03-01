795f7b13f323a0ef56fbacdb58ef75286234114d
Rhullian Damião <rhullian4545@gmail.com>
2022-03-01 16:57:31 -0300

    Initial Commit

---
+++ LearnSwiftUFirst/LearnSwiftUFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUFirstApp.swift
+//  LearnSwiftUFirst
+//
+//  Created by Rhullian Damião on 01/03/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
